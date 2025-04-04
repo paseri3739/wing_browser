a43b45d7cb81ba8fd9d7125d9657fda47048d51d
paseri3739 <tael43@icloud.com>
2025-04-05 03:04:41 +0900

    リロードボタンのロジックを修正。不要な状態を削除しProviderのprogressのみに依存させた。

--- lib/ui_components/top_navigation/reload_button.dart
+++ lib/ui_components/top_navigation/reload_button.dart
@@ -18,12 +18,7 @@ class ReloadButton extends ConsumerStatefulWidget {
 }
 
 class ReloadButtonState extends ConsumerState<ReloadButton> {
-  bool _isLoading = false;
-
   Future<void> _onPressed() async {
-    setState(() {
-      _isLoading = true;
-    });
     // リロード実行
     final webViewState = ref.read(webViewStateProvider);
     await webViewState.webViewController.reload(); // プログレスをプログレスバーのウィジェットが監視することでUIが変更される。
@@ -34,19 +29,10 @@ class ReloadButtonState extends ConsumerState<ReloadButton> {
     final webViewState = ref.watch(webViewStateProvider);
     final progress = webViewState.loadingProgress.progress;
 
-    // 進捗が完了している場合は、ローディング状態を解除
-    if (progress >= 1.0 && _isLoading) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-
     return SquareIconButton(
       height: widget.height,
       // ローディング中はXアイコン、通常はリフレッシュアイコンを表示
-      icon: _isLoading ? Icons.close : Icons.refresh,
+      icon: progress != 0.0 ? Icons.close : Icons.refresh,
       color: widget.reloadButtonColor,
       onPressed: _onPressed,
     );
