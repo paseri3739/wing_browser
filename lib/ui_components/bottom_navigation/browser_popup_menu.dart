c70038207ea9524c4103d5611b34735fadd2db12
paseri3739 <tael43@icloud.com>
2025-04-19 13:40:10 +0900

    fixme

--- lib/ui_components/bottom_navigation/browser_popup_menu.dart
+++ lib/ui_components/bottom_navigation/browser_popup_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// FIXME: PopupMenuItemがクリックされると全然関係ないテキストボックスにフォーカスが当たる。
+// 内部的に新しいフォーカスノードを作成している?
 class BrowserPopupMenu extends StatelessWidget {
   const BrowserPopupMenu({
     super.key,
