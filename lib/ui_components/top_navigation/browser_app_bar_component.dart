6a6f30235ff6f83897c1b44ff62c7ad462edab7e
paseri3739 <tael43@icloud.com>
2025-04-19 23:34:10 +0900

    AppBarのタイトルをFutureBuilderを使用して動的に取得するように変更

--- lib/ui_components/top_navigation/browser_app_bar_component.dart
+++ lib/ui_components/top_navigation/browser_app_bar_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wing_browser/domain/web_view_model.dart';
 
 class BrowserAppBarComponent extends ConsumerWidget implements PreferredSizeWidget {
   final double height;
@@ -12,8 +13,20 @@ class BrowserAppBarComponent extends ConsumerWidget implements PreferredSizeWidg
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final backgroundColor = Theme.of(context).appBarTheme.backgroundColor;
+    final controller = ref.watch(webViewStateProvider).webViewController;
     return AppBar(
-      title: const Text("Tab Bar"),
+      title: FutureBuilder<String?>(
+        future: controller.getTitle(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading...');
+          } else if (snapshot.hasError) {
+            return const Text('Error');
+          } else {
+            return Text(snapshot.data ?? 'No Title');
+          }
+        },
+      ),
       backgroundColor: backgroundColor,
       toolbarHeight: height,
     );
